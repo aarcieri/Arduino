73c61b1daf9461e8694b8df1bf587db0c5731d73
aarcieri <antonio.arcieri@gmail.com>
2021-11-20 18:16:04 +0100

    Add files via upload

--- arduino-timedtasks/main.c
+++ arduino-timedtasks/main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "arduino-timedtasks.h"
+
+int z;
+
+int task_generic(void* args)
+{
+    char* taskName = (char*) args;
+    //printf("\n %s z=%d", taskName,z);
+    z++;
+    return 1;
+}
+
+
+int task1(void* args)
+{
+    //printf("\n Task1");
+    return 0;
+}
+
+int task2(void* args)
+{
+    //printf("\n Task2");
+    add_task(1000, task1, NULL);
+    return 1;
+}
+
+
 
 int main()
 {
-    printf("Hello world!\n");
+    z=0;
+    add_task(1000, task1, NULL);
+    add_task(3000, task2, NULL);
+    add_task(2000, task_generic, "Gino");
+    add_task(80, task_generic,"Pluto");
+
+    printf("\n START ");
+    while(1) tick();
+
     return 0;
 }
